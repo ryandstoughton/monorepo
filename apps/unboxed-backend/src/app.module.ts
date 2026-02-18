96c377d9519a17d1e94084fd6510cdb321aa49b8
Ryan Stoughton <ryandstoughton@gmail.com>
2026-02-17 20:08:53 -0600

    Add docker, linting, and ci for unboxed-backend

--- apps/unboxed-backend/src/app.module.ts
+++ apps/unboxed-backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
