6e0532164e73ca3e087a6d8825c0b4d0f869c73f
Ryan Stoughton <ryandstoughton@gmail.com>
2026-02-17 22:48:57 -0600

    Add dummy call to the backend to verify connectivity

--- apps/unboxed-frontend/src/routes/index.tsx
+++ apps/unboxed-frontend/src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { apiFetch } from "../lib/api";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
+  useEffect(() => {
+    void apiFetch("/users").then((data) => console.log("users:", data));
+  }, []);
+
   return (
     <div>
       <h1>Unboxed</h1>
